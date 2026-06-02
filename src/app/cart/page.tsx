35ed4abbab83998ffdacbd54af9e4aab3d2e5ade
Camilla <camilla.johansson1986@gmail.com>
2026-06-02 13:37:16 +0200

    feat: add homepage, wishlist and cart pages to verify app routing

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,7 @@
+export default function Cart() {
+  return (
+    <>
+      <h1 style={{ margin: '50px auto' }}>Cart</h1>
+    </>
+  );
+}
---
+++ src/app/wishlist/page.tsx
@@ -0,0 +1,7 @@
+export default function Wishlist() {
+  return (
+    <>
+      <h1 style={{ margin: '50px auto' }}>Wishlist</h1>
+    </>
+  );
+}
