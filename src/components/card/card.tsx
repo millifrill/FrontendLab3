9934def795212b630b9edbacdd03ca702f3203d6
Mathias <mathias@Mathias-MacBook.local>
2026-06-03 11:50:44 +0200

    feat: add icons and styling

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,17 +1,33 @@
 import styles from './card.module.css';
+import { IoHeartOutline } from 'react-icons/io5';
+import { IoStar } from 'react-icons/io5';
+import { IoStarHalf } from 'react-icons/io5';
+import { IoStarOutline } from 'react-icons/io5';
 
 function Card(props) {
   return (
-    <div className={styles.container}>
-      <img
-        className={styles.image}
-        src="https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp"
-        alt="placeholderimg"></img>
-      <h2>Mascara</h2>
-      <h3>Gucci</h3>
-      <div className={styles.rating}>★★★★★</div>
-      <p>19,9$</p>
-    </div>
+    <article className={`${styles.container} border border-2 rounded-4`}>
+      <section className="">
+        <IoHeartOutline />
+        <img
+          className={`${styles.image} border-bottom`}
+          src="https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp"
+          alt="placeholderimg"></img>
+      </section>
+      <section className="ms-3 mt-2">
+        <h2 className="fs-6 my-0">Mascara</h2>
+        <p className={`${styles.brand} my-0 text-light`}>Gucci</p>
+        <figure className={`${styles.rating} d-flex gap-1 mb-0`}>
+          <IoStar />
+          <IoStar />
+          <IoStar />
+          <IoStarHalf />
+          <IoStarOutline />
+        </figure>
+
+        <p className="fs-6 my-0 fw-semibold">19,9$</p>
+      </section>
+    </article>
   );
 }
 
