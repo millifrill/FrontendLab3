381462cbd5ff14ba6dab83bb959b460c24f9ce10
Mathias <mathias@Mathias-MacBook.local>
2026-06-04 14:16:28 +0200

    feat: fetch products from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Card from '../components/product-card/product-card';
+import ProductList from '../components/product-list/product-list';
 
 export default function Page() {
   return (
     <>
       <h1 style={{ margin: '30px auto' }}>Welcome!</h1>
       <Card />
+      <ProductList />
     </>
   );
 }
--- src/components/product-list/product-list.tsx
+++ src/components/product-list/product-list.tsx
@@ -1,6 +1,20 @@
 'use client';
+import { useEffect, useState } from 'react';
 import styles from './product-list.module.css';
+import axios from 'axios';
 
 export default function ProductList() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    async function getProducts() {
+      const res = await axios.get('https://dummyjson.com/products');
+      setProducts(res.data.products);
+    }
+    getProducts();
+  }, []);
+  useEffect(() => {
+    console.log(products);
+  }, [products]);
+
   return <></>;
 }
