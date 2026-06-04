86530f9bce022dff6dcf5977ad7f9fcb08e6352e
caroline ackermann <caroline.ackermann@telia.com>
2026-06-04 14:53:47 +0200

    feat: add footer component with ui and navigation links

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigationbar from '../components/navbar/navbar';
 import Footer from '../component/footer/footer';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -10,9 +9,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <html lang='en'>
       <body>
         <Navigationbar />
-        <main style={{ display: 'flex' }}>
-          {children}
-        </main>
+        <main style={{ display: 'flex' }}>{children}</main>
         <Footer />
       </body>
     </html>
--- src/component/footer/footer.tsx
+++ src/component/footer/footer.tsx
@@ -1,37 +1,66 @@
+import Link from "next/link";
 import styles from './footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <img
-        className={styles.brandLogo}
-        alt="Brand logo"
-        src="/images/brand-logo.png"
-      />
-      <p className={styles.p}>Home</p>
-      <p className={styles.p}>About</p>
-      <p className={styles.p}>Contacts</p>
-      <p className={styles.p}>Legal</p>
-      <p className={styles.copyright}>© 2026 Vesti. All rights reserved.</p>
-      <img
-        className={styles.facebookLogo}
-        alt="Facebook"
-        src="/images/facebook.png"
-      />
-      <img
-        className={styles.twitterLogo}
-        alt="Twitter"
-        src="/images/twitter.png"
-      />
-      <img
-        className={styles.youtubeLogo}
-        alt="YouTube"
-        src="/images/youtube.png"
-      />
-      <img
-        className={styles.linkedinLogo}
-        alt="LinkedIn"
-        src="/images/linkedIn.png"
-      />
+      <div className={styles.brand}>
+        <Link href="/">
+        <img
+          className={styles.brandLogo}
+          alt='Brand logo'
+          src='/images/brand-logo.png'
+        />
+        </Link>
+      </div>
+
+      <div className={styles.lingk}>
+        <Link href="/home">
+        <p>Home</p>
+        </Link>
+        <Link href="/about">
+        <p>About</p>
+        </Link>
+        <Link href="/contacts">
+        <p>Contacts</p>
+        </Link>
+         <Link href="/legal">
+        <p>Legal</p>
+        </Link>
+      </div>
+
+      <div className={styles.socialMedia}>
+        <a href='https://www.facebook.com/?locale=sv_SE'>
+          <img
+            className={styles.facebookLogo}
+            alt='Facebook'
+            src='/images/facebook.png'
+          />
+        </a>
+        <a href='https://x.com/?lang=sv'>
+          <img
+            className={styles.twitterLogo}
+            alt='Twitter'
+            src='/images/twitter.png'
+          />
+        </a>
+        <a href='https://www.youtube.com/'>
+          <img
+            className={styles.youtubeLogo}
+            alt='YouTube'
+            src='/images/youtube.png'
+          />
+        </a>
+        <a href='https://se.linkedin.com/'>
+          <img
+            className={styles.linkedinLogo}
+            alt='LinkedIn'
+            src='/images/linkedIn.png'
+          />
+        </a>
+      </div>
+      <div className={styles.copyrightString}>
+        <p className={styles.copyright}>© 2026 Vesti. All rights reserved.</p>
+      </div>
     </footer>
   );
 }
