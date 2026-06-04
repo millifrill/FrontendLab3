b8981911533ee3d004984d8629f3f00b8ed28632
Camilla <camilla.johansson1986@gmail.com>
2026-06-04 21:22:00 +0200

    feat: add offcanvas filter sidebar component with accordion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Page() {
   return (
     <>
       <h1 style={{ margin: '30px auto' }}>Welcome!</h1>
-      <Card />
       <FilterSidebar />
+      <Card />
     </>
   );
 }
--- src/components/filterSidebar/filterSidebar.tsx
+++ src/components/filterSidebar/filterSidebar.tsx
@@ -1,8 +1,11 @@
 'use client';
+import { Accordion, Form } from 'react-bootstrap';
+import { IoStar, IoStarHalf, IoStarOutline } from 'react-icons/io5';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+import styles from './filterSidebar.module.css';
 
 export default function FilterSidebar() {
   return (
@@ -14,10 +17,86 @@ export default function FilterSidebar() {
           </Navbar.Toggle>
           <Navbar.Offcanvas aria-labelledby='Filter' placement='end'>
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title>Filter</Offcanvas.Title>
+              <Offcanvas.Title>Filter & Sort</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-              <Button variant='primary'>Apply Filter</Button>
+              <Accordion alwaysOpen>
+                <Accordion.Item eventKey='0'>
+                  <Accordion.Header>Size</Accordion.Header>
+                  <Accordion.Body>
+                    <p>Sort by size</p>
+                    <Form>
+                      <div className='mb-3'>
+                        <Form.Check type='checkbox' id='XXS' label='XXS' />
+                        <Form.Check type='checkbox' id='XS' label='XS' />
+                        <Form.Check type='checkbox' id='S' label='S' />
+                        <Form.Check type='checkbox' id='M' label='M' />
+                        <Form.Check type='checkbox' id='L' label='L' />
+                        <Form.Check type='checkbox' id='XL' label='XL' />
+                        <Form.Check type='checkbox' id='XXL' label='XXL' />
+                      </div>
+                    </Form>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey='1'>
+                  <Accordion.Header>Price</Accordion.Header>
+                  <Accordion.Body>
+                    <Form.Label>Sort by price range</Form.Label>
+                    <Form.Range />
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey='2'>
+                  <Accordion.Header>Category</Accordion.Header>
+                  <Accordion.Body>
+                    <p>Sort by category</p>
+                    <Form>
+                      <div className='mb-3'>
+                        <Form.Check
+                          type='checkbox'
+                          id='category'
+                          label='Category'
+                        />
+                      </div>
+                    </Form>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey='3'>
+                  <Accordion.Header>Color</Accordion.Header>
+                  <Accordion.Body>
+                    <p>Sort by color</p>
+                    <div className='mb-3'>
+                      <button className={styles.black}></button>
+                      <button className={styles.white}></button>
+                      <button className={styles.yellow}></button>
+                      <button className={styles.green}></button>
+                      <button className={styles.blue}></button>
+                      <button className={styles.purple}></button>
+                      <button className={styles.pink}></button>
+                      <button className={styles.brown}></button>
+                      <button className={styles.beige}></button>
+                    </div>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey='4'>
+                  <Accordion.Header>Raiting</Accordion.Header>
+                  <Accordion.Body>
+                    <p>Sort by raiting</p>
+                    <figure
+                      className={`${styles.rating} d-flex gap-1 my-2 fs-5`}>
+                      <IoStar />
+                      <IoStar />
+                      <IoStar />
+                      <IoStar />
+                      <IoStar />
+                    </figure>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+              <Button
+                className={`mt-3 mx-auto d-block ${styles.applyFilterBtn}`}
+                variant='primary'>
+                Apply Filter
+              </Button>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
         </Container>
