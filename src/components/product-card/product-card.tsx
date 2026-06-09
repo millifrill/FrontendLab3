42bb5cf0a47e754fe1e5aaf78d7bbeef475dd451
Mathias Granlöf <167427463+MathiasGranlof@users.noreply.github.com>
2026-06-09 13:04:57 +0200

    fix: removed icons that will not be used

--- src/components/product-card/product-card.tsx
+++ src/components/product-card/product-card.tsx
@@ -2,9 +2,6 @@ import styles from './product-card.module.css';
 import Card from 'react-bootstrap/Card';
 import ProductInfo from '../product-info/product-info';
 import { IoHeartOutline } from 'react-icons/io5';
-import { HiOutlineChevronLeft } from 'react-icons/hi';
-import { HiOutlineChevronRight } from 'react-icons/hi';
-import { TfiLayoutLineSolid } from 'react-icons/tfi';
 
 export default function ProductCard({ product, smallestPossibleDiscount }) {
   return (
@@ -19,13 +16,7 @@ export default function ProductCard({ product, smallestPossibleDiscount }) {
           <IoHeartOutline
             className={`${styles.heart} fs-1 p-1 bg-dark rounded-circle`}
           />
-          <HiOutlineChevronLeft className='position-absolute fs-3 top-50 start-0 text-dark' />
-          <HiOutlineChevronRight className='position-absolute fs-3 top-50 end-0 text-dark' />
-          <figure className='position-absolute bottom-0 w-100 d-flex justify-content-center gap-1 mb-1'>
-            <TfiLayoutLineSolid />
-            <TfiLayoutLineSolid />
-            <TfiLayoutLineSolid />
-          </figure>
+          <figure className='position-absolute bottom-0 w-100 d-flex justify-content-center gap-1 mb-1'></figure>
           {product.discountPercentage > smallestPossibleDiscount ? (
             <span
               className={`${styles.deal} bg-danger badge rounded-1 ms-1 mb-5 fw-normal`}>
