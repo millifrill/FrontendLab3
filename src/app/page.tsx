e0bfe1c2a379609023bd511893d6cfc9b6ef41ee
Camilla <camilla.johansson1986@gmail.com>
2026-06-12 20:21:13 +0200

    fix: global styling for headings, colors ect for consistent UI and removed redundant code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import ProductList from '../components/product-list/product-list';
 export default function Page() {
   return (
     <>
-      <h1 style={{ margin: '30px auto' }}>Welcome!</h1>
       <ProductList />
     </>
   );
