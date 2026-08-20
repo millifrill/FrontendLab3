9e08c2057d022fbf00dcfecf2f130aa68b2ab4dd
Mathias Granlöf <167427463+MathiasGranlof@users.noreply.github.com>
2026-08-20 11:42:39 +0200

    feat: previewsearch along with various improvements to search functionallity

--- src/components/product-list/product-list.tsx
+++ src/components/product-list/product-list.tsx
@@ -10,11 +10,16 @@ import ProductCard from '../product-card/product-card';
 import FilterSidebar from '../filter-sidebar/filter-sidebar';
 import Pagination from 'react-bootstrap/Pagination';
 import styles from './product-list.module.css';
+import Link from 'next/link';
 
 export default function ProductList() {
   const [searchInput, setSearchInput] = useState<string>('');
   const [searchQuery, setSearchQuery] = useState<string>('');
   const hasSearched = useRef(false);
+  const [previewProducts, setPreviewProducts] = useState<Product[]>([]);
+  const [showPreview, setShowPreview] = useState(false);
+  const searchAndPreview = useRef<HTMLDivElement>(null);
+  const [resultsText, setResultsText] = useState('');
   const [products, setProducts] = useState<Product[]>([]);
   const [pages, setPages] = useState<number>(1);
   const [active, setActive] = useState<number>(1);
@@ -46,11 +51,51 @@ export default function ProductList() {
   }, [active]);
 
   useEffect(() => {
+    if (!searchInput.trim()) {
+      setPreviewProducts([]);
+      return;
+    }
+
+    const previewTimer = setTimeout(async () => {
+      try {
+        const res = await axios.get<ProductRes>(
+          `https://dummyjson.com/products/search?q=${searchInput}&limit=5`,
+        );
+        setPreviewProducts(res.data.products);
+      } catch (error) {
+        console.error('Failed to fetch preview products:', error);
+        setPreviewProducts([]);
+      }
+    }, 500);
+    return () => clearTimeout(previewTimer);
+  }, [searchInput]);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        searchAndPreview.current &&
+        !searchAndPreview.current.contains(e.target as Node)
+      ) {
+        setShowPreview(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
     if (!hasSearched.current) {
       return;
     }
 
     async function getSearchedProducts(): Promise<void> {
+      if (!searchQuery.trim()) {
+        return;
+      }
+      setSearchQuery(searchQuery.trim());
       setLoading(true);
       setError(null);
       try {
@@ -84,89 +129,112 @@ export default function ProductList() {
   }
 
   return (
-    <div className={styles.list}>
-      <div className={styles.searchFilterContainer}>
-        <Form
-          className={styles.searchForm}
-          onSubmit={(e) => {
-            e.preventDefault();
-            hasSearched.current = true;
-            setActive(1);
-            setSearchQuery(searchInput);
-          }}>
-          <InputGroup className='mx-auto w-100'>
-            <Form.Control
-              className={styles.searchFormControl}
-              type='text'
-              placeholder='Search...'
-              onChange={(e) => setSearchInput(e.target.value)}
-            />
-            <InputGroup.Text as='button' type='submit' className='bg-dark'>
-              {loading ? (
-                <Spinner
-                  className={`${styles.loadingIcon} text-light`}
-                  animation='border'
-                  role='status'></Spinner>
-              ) : (
-                <GoSearch className={`${styles.searchIcon} text-light`} />
+    <div>
+      <p className={styles.resultText}>{resultsText}</p>
+      <div className={styles.grid}>
+        <div className={styles.searchFilterContainer}>
+          <Form
+            className={styles.searchForm}
+            onSubmit={(e) => {
+              e.preventDefault();
+              hasSearched.current = true;
+              setActive(1);
+              setSearchQuery(searchInput);
+              setResultsText(
+                searchInput
+                  ? `Search results for "${searchInput.trim()}"`
+                  : resultsText,
+              );
+              setSearchInput('');
+            }}>
+            <InputGroup className='mx-auto w-100' ref={searchAndPreview}>
+              <Form.Control
+                className={styles.searchFormControl}
+                type='text'
+                placeholder='Search...'
+                value={searchInput}
+                onChange={(e) => setSearchInput(e.target.value)}
+                onFocus={() => setShowPreview(true)}
+              />
+              {previewProducts.length >= 0 && showPreview && (
+                <div className={styles.searchPreview}>
+                  {previewProducts.map((product) => (
+                    <Link
+                      key={product.id}
+                      href={`/product-details/${product.id}`}
+                      className={styles.link}>
+                      <div className={styles.previewItem}>
+                        <img src={product.thumbnail} width={50} height={50} />
+                        <strong>{product.title}</strong>
+                      </div>
+                    </Link>
+                  ))}
+                </div>
               )}
-            </InputGroup.Text>
-          </InputGroup>
-        </Form>
-        <FilterSidebar
-          products={products}
-          getProductsByCategory={getProductsByCategory}
-        />
+              <InputGroup.Text as='button' type='submit' className='bg-dark'>
+                {loading ? (
+                  <Spinner
+                    className={`${styles.loadingIcon} text-light`}
+                    animation='border'
+                    role='status'></Spinner>
+                ) : (
+                  <GoSearch className={`${styles.searchIcon} text-light`} />
+                )}
+              </InputGroup.Text>
+            </InputGroup>
+          </Form>
+          <FilterSidebar
+            products={products}
+            getProductsByCategory={getProductsByCategory}
+          />
+        </div>
+        {error ? (
+          <p>{error}</p>
+        ) : products.length > 0 ? (
+          products.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+              smallestPossibleDiscount={5}
+            />
+          ))
+        ) : null}
+        {pages !== 1 ? (
+          <Pagination className={`${styles.pagination} flex-fill`}>
+            <Pagination.Prev
+              className={styles.paginationItem}
+              onClick={() => {
+                setActive(active - 1);
+                window.scrollTo({ top: 0 });
+              }}
+              disabled={active === 1}
+            />
+            {Array.from({ length: pages }, (_, i) => {
+              const num = i + 1;
+              return (
+                <Pagination.Item
+                  className={styles.paginationItem}
+                  key={num}
+                  active={num === active}
+                  onClick={() => {
+                    setActive(num);
+                    window.scrollTo({ top: 0 });
+                  }}>
+                  {num}
+                </Pagination.Item>
+              );
+            })}
+            <Pagination.Next
+              className={styles.paginationItem}
+              onClick={() => {
+                setActive(active + 1);
+                window.scrollTo({ top: 0 });
+              }}
+              disabled={active === pages}
+            />
+          </Pagination>
+        ) : null}
       </div>
-      {hasSearched && !loading && products.length === 0 ? (
-        <p>No results...</p>
-      ) : null}
-      {error ? (
-        <p>{error}</p>
-      ) : products.length > 0 ? (
-        products.map((product) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            smallestPossibleDiscount={5}
-          />
-        ))
-      ) : null}
-      {products.length > 0 ? (
-        <Pagination className={`${styles.pagination} flex-fill`}>
-          <Pagination.Prev
-            className={styles.paginationItem}
-            onClick={() => {
-              setActive(active - 1);
-              window.scrollTo({ top: 0 });
-            }}
-            disabled={active === 1}
-          />
-          {Array.from({ length: pages }, (_, i) => {
-            const num = i + 1;
-            return (
-              <Pagination.Item
-                className={styles.paginationItem}
-                key={num}
-                active={num === active}
-                onClick={() => {
-                  setActive(num);
-                  window.scrollTo({ top: 0 });
-                }}>
-                {num}
-              </Pagination.Item>
-            );
-          })}
-          <Pagination.Next
-            className={styles.paginationItem}
-            onClick={() => {
-              setActive(active + 1);
-              window.scrollTo({ top: 0 });
-            }}
-            disabled={active === pages}
-          />
-        </Pagination>
-      ) : null}
     </div>
   );
 }
